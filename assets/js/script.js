6216f376e68e60add08c632123c5d5290da0afc1
Carl <carlelli5@hotmail.co.uk>
2022-09-30 10:25:35 +0000

    Added division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion (num1, num2)
     } else if (gameType === "subtract") {
         displaySubtractQuestion (num1, num2);
     } else if (gameType === "multiply") {
@@ -86,6 +88,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"]
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
@@ -136,4 +140,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 * operand2;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
